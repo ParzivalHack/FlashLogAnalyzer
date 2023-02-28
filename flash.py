eb22c267384262db6c45f61239182fda981a340f
Tommaso Bona <piergeolo@gmail.com>
2023-02-28 12:08:43 +0100

    Update flash.py

--- flash.py
+++ flash.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import filedialog
 import re
+from collections import Counter
+import os
 
 # Define the functions for parsing and analyzing log files
 def parse_logs(logfile):
@@ -19,30 +21,21 @@ def parse_logs(logfile):
     times = re.findall(time_regex, log_contents)
 
     # Zip the matches into tuples for easier processing
-    log_entries = zip(ips, dates, times)
+    log_entries = list(zip(ips, dates, times))
 
     # Return the log entries as a list
-    return list(log_entries)
+    return log_entries
 
 def analyze_logs(log_entries):
-    # Define a dictionary to store the number of occurrences for each IP address
-    ip_counts = {}
-
-    # Iterate over each log entry and count the number of occurrences for each IP address
-    for entry in log_entries:
-        ip_address = entry[0]
-        if ip_address in ip_counts:
-            ip_counts[ip_address] += 1
-        else:
-            ip_counts[ip_address] = 1
+    # Use Counter to count the occurrences of each IP address
+    ip_counts = Counter(entry[0] for entry in log_entries)
 
     # Sort the IP addresses by the number of occurrences in descending order
-    sorted_ips = sorted(ip_counts, key=ip_counts.get, reverse=True)
+    sorted_ips = sorted(ip_counts.items(), key=lambda x: x[1], reverse=True)
 
     # Return the sorted IP addresses and their corresponding occurrence counts
-    return [(ip, ip_counts[ip]) for ip in sorted_ips]
+    return sorted_ips
 
-# Define the function for the GUI
 def analyze_logs_gui():
     # Create a new window
     window = tk.Tk()
@@ -50,13 +43,22 @@ def analyze_logs_gui():
     # Define a function for opening log files
     def open_file():
         filename = filedialog.askopenfilename(initialdir='/', title='Select file', filetypes=(('Text files', '*.txt'), ('All files', '*.*')))
-        filename_entry.delete(0, tk.END)
-        filename_entry.insert(0, filename)
+        if filename:
+            filename_entry.delete(0, tk.END)
+            filename_entry.insert(0, filename)
+            analyze_button.config(state=tk.NORMAL)
 
     # Define a function for analyzing log files
     def analyze_file():
         logfile = filename_entry.get()
-        log_entries = parse_logs(logfile)
+        if not logfile:
+            messagebox.showerror('Error', 'Please select a log file')
+            return
+        try:
+            log_entries = parse_logs(logfile)
+        except Exception as e:
+            messagebox.showerror('Error', f'Error parsing log file: {str(e)}')
+            return
         security_incidents = analyze_logs(log_entries)
 
         # Create a new window to display the results
@@ -83,7 +85,7 @@ def analyze_logs_gui():
     select_button.grid(row=0, column=2)
 
     # Create a button for analyzing the log file
-    analyze_button = tk.Button(window, text='Analyze', command=analyze_file)
+    analyze_button = tk.Button(window, text='Analyze', command=analyze_file, state=tk.DISABLED)
     analyze_button.grid(row=1, column=1)
     
     window.mainloop()
